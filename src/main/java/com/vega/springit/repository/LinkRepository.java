4fd24d4cd9b2b131fda102bb4a969a9c8ffe10ab
nsl0707test <75154397+nsl0707test@users.noreply.github.com>
2021-01-09 06:58:04 +0530

    commited out commandline runner.

--- src/main/java/com/vega/springit/repository/LinkRepository.java
+++ src/main/java/com/vega/springit/repository/LinkRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
-    Link findByTitle(String title);
-    List<Link> findAllByTitleLikeOrderByCreationDateDesc(String title);
+//    Link findByTitle(String title);
+//    List<Link> findAllByTitleLikeOrderByCreationDateDesc(String title);
 
+    Link findByTitle(String title);
+
+    List<Link> findAllByTitleLikeOrderByCreationDateDesc(String title);
 }
